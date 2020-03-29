9292636613d80f6a6e8dfa4dd58447a59ac224a0
TheYoungSatan <43784009+TheYoungSatan@users.noreply.github.com>
2020-03-29 10:39:06 +0200

    Added particle placeholder

--- GameArt3_TheCrazyBrawler/Assets/Essentials/Scripts/PlayerScripts/PunchHitDetection.cs
+++ GameArt3_TheCrazyBrawler/Assets/Essentials/Scripts/PlayerScripts/PunchHitDetection.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 public class PunchHitDetection : MonoBehaviour
 {
     public event Action<GameObject, Vector3> OnHit;
+    [SerializeField] private Transform _instantiationPointParticles;
+    [SerializeField] private GameObject _particle;
+    [SerializeField] private float _particleDuration;
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.transform.parent != transform.root && other.CompareTag("hitbox") && !other.transform.root.CompareTag(transform.root.tag)) 
+        if (other.transform.parent != transform.root && other.CompareTag("hitbox") && !other.transform.root.CompareTag(transform.root.tag))
+        { 
             OnHit?.Invoke(other.transform.root.gameObject, transform.root.forward);
+            GameObject obj = Instantiate(_particle, _instantiationPointParticles);
+            obj.transform.position = _instantiationPointParticles.transform.position;
+            obj.transform.parent = null;
+            Destroy(obj, _particleDuration);
+        }
     }
 }
